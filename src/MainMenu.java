e1b2ee848c3a590349f75709726304fa11ba2150
TannerCoker <tbc53@txstate.edu>
2020-10-22 15:35:07 -0500

    adding MainMenu.java class

---
+++ src/MainMenu.java
@@ -0,0 +1,12 @@
+/*
+Author: Tanner Coker
+
+This is a class that displays that main menu for the game SpaceAdventure 3398
+*/
+import java.awt.*;
+import java.swing.*;
+
+public class MainMenu extends jPanel()
+{
+  
+}
