91c7bdf4b7f13a0326616c660c1bab54a4be2960
TannerCoker <tbc53@txstate.edu>
2020-10-27 14:52:08 -0500

    added the images to the main menu screen. needs to be tweaked but fine for testing purposes

--- src/Background.java
+++ src/Background.java
@@ -1,12 +1,12 @@
 /*
  * Author: Tanner Coker
- * 
+ *
  * Background is a background that will display a black square on which everything else will
- * lay on top of. It currently has a UpdateThread that will update the location of all of the 
+ * lay on top of. It currently has a UpdateThread that will update the location of all of the
  * stars and planets on it.
  */
 
-import javax.swing.*; 
+import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 import java.io.*;
@@ -17,32 +17,32 @@ public class Background extends JPanel
 	private int height = JFrame.MAXIMIZED_VERT;
 	private int frameWidth;
 	private ArrayList<SpaceBodies> bodies = new ArrayList<SpaceBodies>();
-	
+
 
 	//adds stars and planets to an arraylist of type SpaceBodies
-	//also makes a UpdateThread that will call Update() 
+	//also makes a UpdateThread that will call Update()
 	public Background()
 	{
-	
+
 		//changes the amount of stars
 		for(int i=0;i<500;i++)
 		{
 			Stars s = new Stars();
 			bodies.add(s);
 		}
-		
+
 		//changes the amount of planets
 		for(int i=0;i<7;i++)
 		{
 			Planets p = new Planets();
 			bodies.add(p);
 		}
-		
+
 		//thread to run update
 		UpdateBG ub = new UpdateBG(this);
 		ub.start();
 	}
-	
+
 	//passes the width of the frame to the stars and planets
 	public void giveFrameWidth(int w)
 	{
@@ -51,7 +51,7 @@ public class Background extends JPanel
 			sb.setFrameWidth(w);
 		}
 	}
-	
+
 	//updates the stars and planets and causes them to update their position
 	public void update()
 	{
@@ -61,7 +61,7 @@ public class Background extends JPanel
 		}
 		repaint();
 	}
-	
+
 	//paints the black background and all of the stars and planets
 	@Override
 	public void paintComponent(Graphics g)
@@ -69,7 +69,7 @@ public class Background extends JPanel
 		super.paintComponent(g);
 		g.setColor(Color.black);
 		g.fillRect(0, 0, getWidth(), getHeight());
-		
+
 		for(SpaceBodies sb : bodies)
 		{
 			sb.draw(g);
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,9 +4,49 @@ Author: Tanner Coker
 This is a class that displays that main menu for the game SpaceAdventure 3398
 */
 import java.awt.*;
-import java.swing.*;
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
 
-public class MainMenu extends jPanel()
+public class MainMenu extends JPanel
 {
-  
+  Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+  int width = screenSize.width;
+  int height = screenSize.height;
+  ImageIcon title, play, score, settings, about, exit;
+  MenuButtons playButton, scoreButton, settingsButton, aboutButton, exitButton;
+
+  public MainMenu()
+  {
+    title = new ImageIcon("../images/Title.png");
+    play = new ImageIcon("../images/possible_play_button.png");
+    score = new ImageIcon("../images/Scoreboard.png");
+    settings = new ImageIcon("../images/Settings.png");
+    about = new ImageIcon("../images/About.png");
+    exit = new ImageIcon("../images/Exit.png");
+
+    playButton = new MenuButtons(0,0);
+    scoreButton = new MenuButtons(0,50,25);
+    settingsButton = new MenuButtons(0,100);
+    aboutButton = new MenuButtons(0,150);
+    exitButton = new MenuButtons(0,200);
+
+    playButton.setPicture(play);
+    scoreButton.setPicture(score);
+    settingsButton.setPicture(settings);
+    aboutButton.setPicture(about);
+    exitButton.setPicture(exit);
+  }
+
+  public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g.setColor(Color.black);
+		g.fillRect(0, 0, getWidth(), getHeight());
+    playButton.draw(g,this);
+    scoreButton.draw(g,this);
+    settingsButton.draw(g,this);
+    aboutButton.draw(g,this);
+    exitButton.draw(g,this);
+	}
 }
