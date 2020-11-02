84cae910f80f24aff3d720190e9768e625c55c20
Eric Figueroa <eaf61@txstate.edu>
2020-11-02 13:01:07 -0600

    Moving player/enemy classes.

--- src/SpacePlayer.java
+++ src/SpacePlayer.java
@@ -1,24 +1,28 @@
-package game.objects;
+/*
+ * Author: Eric Figueroa
+ * SpacePlayer is the player of the game that will be fighting against SpaceEnemy by shooting
+ * enemy ships to move onto the next level of the game. 
+ */
 
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.io.IOException;
 
-import game.SpaceAdventures;
-// Space player is the player of the game
 public class SpacePlayer extends GameObj{
 	private int score = 0;
-	
+
+	// creation of player using image we have created	
 	public SpacePlayer(int x, int y, SpaceAdventures game) {
 		super(x, y, game);
+	// try catch to load image
 		try {
 			this.setImage(getImage().getImageLoader().loadImage("/player-ship1.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	// prints graphics to the screen by setting X/Y with bounds
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(getImage(), getX(), getY();
