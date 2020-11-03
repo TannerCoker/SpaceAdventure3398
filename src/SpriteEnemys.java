0f42d4cb7bb23a1a8dbd14126ba8938bad833fc7
Eric Figueroa <eaf61@txstate.edu>
2020-11-02 22:41:04 -0600

    updating sprite classes needed for player and enemy.

--- src/SpriteEnemys.java
+++ src/SpriteEnemys.java
@@ -1,5 +1,23 @@
-/* 
+/*
  * Eric Figueroa
- * SpriteEnemys. java is the class needed to create enemy players.
- * 
+ * SpriteEnemys.java is the class needed to create enemy players. 
+ *
  */
+package maingame;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteEnemys {
+	
+	private BufferedImage image;
+	
+	public SpriteEnemys(BufferedImage image) {
+		this.image = image;
+	}
+	
+	public BufferedImage grabImage(int col, int row, int width, int height) {
+		BufferedImage img = image.getSubimage((col * 32) - 32,(row * 32) - 32, width, height);
+		return img;
+	}
+
+}
--- src/SpritePlayers.java
+++ src/SpritePlayers.java
@@ -1,5 +1,23 @@
-/* 
+/*
  * Eric Figueroa
- * SpritePlayers.java is the class needed to help create players.
+ * SpritePlayers.java is the class needed to help create players
  *
- */ 
+ */
+package maingame;
+
+import java.awt.image.BufferedImage;
+
+public class SpritePlayers {
+	
+	private BufferedImage image;
+	
+	public SpritePlayers(BufferedImage image) {
+		this.image = image;
+	}
+	
+	public BufferedImage grabImage(int col, int row, int width, int height) {
+		BufferedImage img = image.getSubimage((col * 32) - 32,(row * 32) - 32, width, height);
+		return img;
+	}
+
+}
