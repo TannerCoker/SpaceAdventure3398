f33bed009e634c870610e0367f3043d216e4ce51
Erik Cortez <elc44@txstate.edu>
2020-11-02 21:46:52 -0600

    added line

--- src/ShipProjHitbox.java
+++ src/ShipProjHitbox.java
@@ -2,6 +2,9 @@ import java.awt.Image;
 
 public class projToShipCol extends Sprite {
 
+
+	private Game game;
+
 	public void projToShipCol(Sprite ship) {
 		if (ship instanceof AlienShip) {
 			game.shotSpriteRem(this);
