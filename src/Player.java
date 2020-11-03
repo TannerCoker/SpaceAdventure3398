841e0d170c35fc551babc622c999050c820947a1
Eric Figueroa <eaf61@txstate.edu>
2020-11-02 22:39:55 -0600

    updating/tweaking player and enemy classes.

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,33 @@
 /*
  * Eric Figueroa
- * Player.java is the players of Space Adventures 3398.
- * 
+ * Player.java is the player of space-adventure-3398
+ *
  */
+package maingame;
+
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Player {
+	
+	private double x;
+	private double y;
+	private BufferedImage player;
+	
+	public Player(double x, double y, Game game) {
+		this.x = x;
+		this.y = y;
+		
+		SpritePlayers ss = new SpritePlayers(game.getSpritePlayers());
+		player = ss.grabImage(1, 1, 600, 600);
+	}
+	
+	public void tick() {
+		x++;
+	}
+	public void render(Graphics g) {
+		g.drawImage(player, (int)x, (int)y, null);
+	}
+
+}
