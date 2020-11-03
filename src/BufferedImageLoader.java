3a85f99272a81da8ccf48a9f4c22227102cc9af0
Eric Figueroa <eaf61@txstate.edu>
2020-11-02 22:51:16 -0600

    updating the BufferLoaderImage class.

--- src/BufferedImageLoader.java
+++ src/BufferedImageLoader.java
@@ -1,6 +1,22 @@
-/*
+/* 
  * Eric Figueroa
- * BufferedImageLoader.java is the class needed to upload .png images to
- * screen and background.
- *
+ * BufferedImageLoader.java is the class needed to upload image files for
+ * player and enemy to screen and background.
  */
+package maingame;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class BufferedImageLoader {
+	
+	private BufferedImage image;
+	
+	public BufferedImage loadImage(String path) throws IOException {
+		
+		image = ImageIO.read(getClass().getResource(path));
+		return image;
+	}
+
+}
