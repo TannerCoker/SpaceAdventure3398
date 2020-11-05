1dc42a210c8aa13891e1a8bbcdc1894eaf7eb726
Eric Figueroa <eaf61@txstate.edu>
2020-11-04 21:50:11 -0600

    adding to do accurate tests run of player/enemy code.

---
+++ src/SpaceAdv3398/src/maingame/SpriteEnemys.java
@@ -0,0 +1,18 @@
+package maingame;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteEnemys {
+	
+	private BufferedImage image;
+	
+	public SpriteEnemys(BufferedImage image) {
+		this.image = image;
+	}
+	
+	public BufferedImage grabImage(int col, int row, int width, int height) {
+		BufferedImage img = image.getSubimage((col * 32) - 32,(row * 32) - 32, width, height);
+		return img;
+	}
+
+}
